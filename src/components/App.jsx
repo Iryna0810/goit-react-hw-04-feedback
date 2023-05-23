9a34abc3b97aee8ce0985dd1c4cfaaf2521e9f8e
Rew <irinavakulenko2000@gmail.com>
2023-05-24 00:07:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { Title } from "./Title/Title";
 
 
 
-export function App () {
+export const App = () => {
 
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
